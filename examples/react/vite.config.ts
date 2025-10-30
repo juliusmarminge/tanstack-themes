c553ed227abec834fd53a869e52b9569ca348ca1
Julius <julius0216@outlook.com>
2025-10-29 21:28:28 -0700

    nitro

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -2,10 +2,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [tailwindcss(), tanstackStart(), viteReact(), nitro()],
 });
