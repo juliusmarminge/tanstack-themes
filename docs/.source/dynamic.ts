6eaeee6c1c70fcc69bafce08aa9695dcb8861ac2
Julius <julius0216@outlook.com>
2025-11-16 19:30:23 -0800

    ig

--- docs/.source/dynamic.ts
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-/// <reference types="vite/client" />
-import { fromConfigDynamic } from 'fumadocs-mdx/runtime/dynamic';
-import * as Config from '../source.config';
-
-const create = await fromConfigDynamic(Config);
