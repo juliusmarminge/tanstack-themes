a9e43800bca20a04d037dfe66ef08d0db77ac3a6
Julius <julius0216@outlook.com>
2025-11-16 19:58:09 -0800

    clean

--- docs/src/routes/__root.tsx
+++ docs/src/routes/__root.tsx
@@ -6,7 +6,7 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import * as React from "react";
-import appCss from "@/styles/app.css?url";
+import appCss from "@/styles.css?url";
 import { RootProvider } from "fumadocs-ui/provider/tanstack";
 
 export const Route = createRootRoute({
